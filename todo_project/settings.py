2ab64bed64a2d4d2c6bf519bf79f03ff9fa6f948
elmorem <elmorem@gmail.com>
2020-02-27 13:13:31 -0800

    added rest_framework permissions

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -38,10 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+#3rd party
+'rest_framework',
 
    #local
    'todos.apps.TodosConfig', 
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
